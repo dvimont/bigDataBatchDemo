93520df76901aeab749eb90adf4ad68aced7ed48
dv <dv@dv-VirtualBox>
2017-10-01 20:48:14 -0400

    Reorganization of project directories; moved old unused source code to scr_archive; moved all scripts to src/main/bin.

--- src/main/java/org/commonvox/bigdatademos/SparkDriver.java
+++ src/main/java/org/commonvox/bigdatademos/SparkDriver.java
@@ -170,7 +170,6 @@ public class SparkDriver {
                         .mapToPair(new JsonMapper())
                 ;
         yearlyPagesByPopularity.saveAsTextFile(hdfsNamenode + outputYearlyHdfsFile);
-        
     }
     
     static class DailyMapper implements Function2<InputSplit,
